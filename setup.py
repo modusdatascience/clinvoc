d6a41176fe3066cee6b2d40b5e443486b9f4b5b4
Jason Rudy <jcrudy@gmail.com>
2017-03-29 10:58:21 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='clinvoc',
+      version='0.1',
+      author='Jason Rudy',
+      author_email='jcrudy@gmail.com',
+      url='https://github.com/jcrudy/clinvoc',
+      packages=['clinvoc'],
+      requires=[]
+     )
