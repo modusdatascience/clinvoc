1b26597362e0dd9ebce4b7a1f0c0f8f3e0b60735
Jason Rudy <jcrudy@gmail.com>
2017-04-16 14:05:22 -0700

    Use pep440 versioning

--- clinvoc/_version.py
+++ clinvoc/_version.py
@@ -40,7 +40,7 @@ def get_config():
     # _version.py
     cfg = VersioneerConfig()
     cfg.VCS = "git"
-    cfg.style = "git-describe-long"
+    cfg.style = "pep440"
     cfg.tag_prefix = ""
     cfg.parentdir_prefix = "clinvoc"
     cfg.versionfile_source = "clinvoc/_version.py"
