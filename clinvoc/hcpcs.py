5393dcefc626cb81c191e0a3405cde925f5bc803
Jason Rudy <jcrudy@gmail.com>
2017-04-28 15:57:07 -0700

    Working on upgrading hcpcs

--- clinvoc/hcpcs.py
+++ clinvoc/hcpcs.py
@@ -1,5 +1,6 @@
-from .base import SimpleParseVocabulary
+from .base import RegexVocabulary, left_pad
 import re
+from clinvoc.base import NoWildcardsVocabulary, NoRangeFillVocabulary
 
 _hcpcs_split_regex = re.compile('([A-z]*)([0-9]+)')
 def hcpcs_split(code):
@@ -12,9 +13,12 @@ def hcpcs_join(letter_part, number_part):
     digits = 5 - len(letter_part)
     return letter_part + (('%%.%dd' % digits) % int(number_part))
 
-
-
-class HCPCS(SimpleParseVocabulary):
+class HCPCS(RegexVocabulary, NoWildcardsVocabulary):
+    def __init__(self):
+        level_1_regex = '(\d{4}F)|(\d{4}T)|(\d{1,5})'
+        level_2_regex = '[A-V]\d{4}'
+        RegexVocabulary.__init__(self, '(%s)|(%s)' % (level_1_regex, level_2_regex))
+        
     @staticmethod
     def _fill_range(start, end):
         start_letter, start_number = hcpcs_split(start)
@@ -26,26 +30,15 @@ class HCPCS(SimpleParseVocabulary):
         return result
     
     @staticmethod
-    def _match_pattern(pattern):
-        return [pattern]
-    
-    @staticmethod
-    def standardize(code):
-        result = code.strip()
-        assert len(result) == 5
-        return result
+    def _standardize(code):
+        return left_pad(code.strip().upper(), 5)
 
-class HCPCSModifier(SimpleParseVocabulary):
-    @staticmethod
-    def _fill_range(start, end):
-        return [start, end]
+class HCPCSModifier(RegexVocabulary, NoWildcardsVocabulary, NoRangeFillVocabulary):
+    def __init__(self):
+        RegexVocabulary.__init__(self, '[A-Za-z\d]{2}')
     
     @staticmethod
-    def _match_pattern(pattern):
-        return [pattern]
-    
-    @staticmethod
-    def standardize(code):
+    def _standardize(code):
         result = code.strip()
         assert len(result) == 2
         return result
