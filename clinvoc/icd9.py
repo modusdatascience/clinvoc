e9f98ce9bafbc11e1d8ddec2115af3574016ab43
Jason Rudy <jcrudy@gmail.com>
2017-04-28 14:00:51 -0700

    Upgrade icd9-cm

--- clinvoc/icd9.py
+++ clinvoc/icd9.py
@@ -1,91 +1,65 @@
 import os
 from .resources import resources
-from .base import create_bisection_range_filler, create_fnmatch_wildcard_matcher, SimpleParseVocabulary
+from .base import SimpleParseVocabulary, left_pad
+from clinvoc.base import LexicographicPatternMatchVocabulary,\
+    LexicographicRangeFillVocabulary, LexicographicVocabulary, \
+    RegexVocabulary
 
-def parse_dx_code(code):
-    code = code.upper()
-    assert code[0] != ' '
-    code = code.strip()
-    if code[0] == 'V':
-        result = code[:3]
-        if len(code) > 3:
-            result += '.' + code[3:]
-    elif code[0] == 'E':
-        result = code[:4]
-        if len(code) > 4:
-            result += '.' + code[3:]
-    else:
-        assert code[0].isdigit()
-        result = code[:3] 
-        if len(code) > 3:
-            result += '.' + code[3:]
-    return result
-
-def read_dx_text_file(filename):
-    result = {}
+def _read_text_file(filename):
+    result = []
     with open(filename, 'rb') as infile:
         for line in infile:
-            code = line[:5]
-            desc = line[6:].strip()
-            result[parse_dx_code(code)] = desc
+            result.append(line[:5])
     return result
 
-def parse_px_code(code):
-    code = code.upper()
-    assert code[0] != ' '
-    code = code.strip()
-    if code[0] == 'V':
-        raise ValueError('code')
-        result = code[:3]
-        if len(code) > 3:
-            result += '.' + code[3:]
-    elif code[0] == 'E':
-        raise ValueError('code')
-        result = code[:4]
-        if len(code) > 4:
-            result += '.' + code[3:]
-    else:
-        assert code[0].isdigit()
-        result = code[:2] 
-        if len(code) > 2:
-            result += '.' + code[2:]
-    return result
+_all_icd9_cm_codes = _read_text_file(os.path.join(resources, 'CMS32_DESC_SHORT_DX.txt'))
+_all_icd9_pcs_codes = _read_text_file(os.path.join(resources, 'CMS32_DESC_SHORT_SG.txt'))
 
-def read_px_text_file(filename):
-    result = {}
-    with open(filename, 'rb') as infile:
-        for line in infile:
-            code = line[:5]
-            desc = line[6:].strip()
-            result[parse_px_code(code)] = desc
-    return result
-
-# 
-# 
-# all_icd9_dx_codes = read_dx_text_file(os.path.join(resources, 'CMS32_DESC_SHORT_DX.txt')).keys()
-# all_icd9_dx_codes.sort()
-# all_icd9_px_codes = read_px_text_file(os.path.join(resources, 'CMS32_DESC_SHORT_SG.txt')).keys()
-# all_icd9_px_codes.sort()
-# 
-# icd9_dx_range_fill = create_bisection_range_filler(all_icd9_dx_codes)
-# icd9_px_range_fill = create_bisection_range_filler(all_icd9_px_codes)
-
-class ICD9PCS(SimpleParseVocabulary):
-    all_icd9_px_codes = read_px_text_file(os.path.join(resources, 'CMS32_DESC_SHORT_SG.txt')).keys()
-    all_icd9_px_codes.sort()
-    _fill_range = staticmethod(create_bisection_range_filler(all_icd9_px_codes, '_fill_range'))
-    _match_pattern = staticmethod(create_fnmatch_wildcard_matcher(all_icd9_px_codes, '_match_pattern'))
-    def standardize(self, code):
-        result = code.strip()
+class ICD9CM(RegexVocabulary, LexicographicPatternMatchVocabulary, LexicographicRangeFillVocabulary):
+    def __init__(self):
+        v_regex = 'V[\d\*]{2}((\.[\d\*]{1,2})|([\d\*]{0,2}))'
+        e_regex = 'E[\d\*]{3}((\.[\d\*]{1})|([\d\*]{0,1}))'
+        n_regex = '[\d\*]{1,3}((\.[\d\*]{1,2})|([\d\*]{0,2}))'
+        RegexVocabulary.__init__(self, '(%s)|(%s)|(%s)' % (v_regex, e_regex, n_regex))
+        LexicographicVocabulary.__init__(self, _all_icd9_cm_codes)
+        
+    def _standardize(self, code):
+        code_ = code.strip().upper()
+        if '.' in code_:
+            pre, post = code_.split('.')
+            result = left_pad(pre, 3) + '.' + post
+        else:
+            if code_[0] == 'V':
+                result = code_[:3]
+                if len(code_) > 3:
+                    result += '.' + code_[3:]
+            elif code_[0] == 'E':
+                result = code_[:4]
+                if len(code_) > 4:
+                    result += '.' + code_[4:]
+            else:
+                assert code_[0].isdigit()
+                result = code_[:3] 
+                if len(code_) > 3:
+                    result += '.' + code_[3:]
         return result
 
-class ICD9CM(SimpleParseVocabulary):
-    all_icd9_dx_codes = read_dx_text_file(os.path.join(resources, 'CMS32_DESC_SHORT_DX.txt')).keys()
-    all_icd9_dx_codes.sort()
-    _fill_range = staticmethod(create_bisection_range_filler(all_icd9_dx_codes, '_fill_range'))
-    _match_pattern = staticmethod(create_fnmatch_wildcard_matcher(all_icd9_dx_codes, '_match_pattern'))
+class ICD9PCS(SimpleParseVocabulary, LexicographicPatternMatchVocabulary, LexicographicRangeFillVocabulary):
+    def __init__(self):
+        LexicographicVocabulary.__init__(self, _all_icd9_pcs_codes)
+
     def standardize(self, code):
-        result = code.strip()
+        code_ = code.strip().upper()
+        if '.' in code_:
+            pre, post = code_.split('.')
+            result = left_pad(pre, 2) + '.' + post
+        else:
+            assert code_[0].isdigit()
+            result = code_[:2] 
+            if len(code_) > 2:
+                result += '.' + code_[2:]
         return result
+
+
 
 
