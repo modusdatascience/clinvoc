d4a3c40253913031b9303e626e1b7ec2e02531f9
Jason Rudy <jcrudy@gmail.com>
2017-04-27 20:51:46 -0700

    WGet NDC working

--- clinvoc/base.py
+++ clinvoc/base.py
@@ -3,6 +3,10 @@ from bisect import bisect_left
 from abc import abstractmethod
 import fnmatch
 import re
+from toolz.functoolz import memoize
+from pyparsing import Regex, NoMatch, Literal, White, ZeroOrMore, StringEnd
+from operator import or_
+from itertools import product, chain
 
 def left_pad(code, expected_length, padding='0'):
     n = len(code)
@@ -18,72 +22,40 @@ def index(a, x):
     raise ValueError
 
 def create_bisection_range_filler(codes, name=None):
+    sorted_codes = sorted(codes)
     def range_filler(start, end):
-        left_idx = index(codes, start)
-        right_idx = index(codes, end)
-        return [codes[idx] for idx in range(left_idx, right_idx + 1)]
+        left_idx = index(sorted_codes, start)
+        right_idx = index(sorted_codes, end)
+        return [sorted_codes[idx] for idx in range(left_idx, right_idx + 1)]
     if name is not None:
         range_filler.__name__ = name
     return range_filler
 
 def create_fnmatch_wildcard_matcher(codes, name=None):
+    sorted_codes = sorted(codes)
     def fnmatch_wildcard_matcher(pattern):
-        return fnmatch.filter(codes, pattern)
+        return fnmatch.filter(sorted_codes, pattern)
     
     if name is not None:
         fnmatch_wildcard_matcher.__name__ = name
     return fnmatch_wildcard_matcher
 
+def create_vocabulary_checker(codes, name=None):
+    code_set = set(codes)
+    def check(code):
+        return code in code_set
+    if name is not None:
+        check.__name__ = str(name)
+    return check
+
 class Vocabulary(object):
     @abstractmethod
-    def parse(self, expression, delimiter=',;\s', range_delimiter='-'):
-        delimiter_pattern = re.compile('[^%s]+' % delimiter)
-        range_delimiter_pattern = re.compile(range_delimiter)
-        
-        # Remove any whitespace around range delimiters
-        delimiter_range_delimiter_reduction_pattern = re.compile('\s*%s\s*' % range_delimiter)
-        reduced_expression = delimiter_range_delimiter_reduction_pattern.sub(range_delimiter, expression)
-        
-        # Break into parts by delimiter
-        parts = delimiter_pattern.findall(reduced_expression)
-        
-        # Now look for any ranges or wild cards
-        codes = set()
-        for raw_part in parts:
-            # Remove quotes
-            part = raw_part.replace('\'', '').replace('"', '').replace('’', '').replace('‘', '')
-            
-            # Handle ranges and patterns
-            if range_delimiter_pattern.search(part):
-                raw_start, raw_end = re.split("'?\s*%s\s*'?" % range_delimiter, part)
-                starts = self.match_pattern(raw_start)
-                assert starts
-                ends = self.match_pattern(raw_end)
-                assert ends
-                for start in starts:
-                    for end in ends:
-                        codes.update(self.fill_range(start, end))
-            else:
-                matches = self.match_pattern(part)
-                assert matches
-                codes.update(matches)
-        return codes
-#                 
-#                 
-#                 if '*' in start:
-#                     starts = wild_card_fillers[(code_type, code_system)](start)
-#                 else:
-#                     starts = [start]
-#                 if '*' in end:
-#                     ends = wild_card_fillers[(code_type, code_system)](end)
-#                 else:
-#                     ends = [end]
-#                 codes.extend(list(set(starts+ends+range_fillers[(code_type, code_system)](min(starts), max(ends)))))
-#             elif '*' in part:
-#                 codes.extend(wild_card_fillers[(code_type, code_system)](part))
-#             else:
-#                 codes.append(part)
-#         return [code_processors[(code_type, code_system)](code) for code in codes]
+    def parse(self, expression):
+        raise NotImplementedError
+    
+    @abstractmethod
+    def _match_pattern(self, pattern):
+        raise NotImplementedError
     
     @abstractmethod
     def standardize(self, code):
@@ -93,15 +65,150 @@ class Vocabulary(object):
     def _fill_range(self, lower, upper):
         raise NotImplementedError
     
-    def fill_range(self, lower, upper):
-        return [self.standardize(code) for code in self._fill_range(self.standardize(lower), self.standardize(upper))]
-    
     @abstractmethod
-    def _match_pattern(self, pattern):
+    def check(self, code):
         raise NotImplementedError
     
     def match_pattern(self, pattern):
-        return [self.standardize(code) for code in self._match_pattern(self.standardize(pattern))]
+        return frozenset([self.standardize(code) for code in self._match_pattern(self.standardize(pattern))])
+    
+    def strict_parse(self, expression, *args, **kwargs):
+        return self.filter(self.parse(expression, *args, **kwargs))
+    
+    def fill_range(self, lower, upper):
+        return [self.standardize(code) for code in self._fill_range(self.standardize(lower), self.standardize(upper))]
+    
+    def fill_set_range(self, lowers, uppers):
+        result = set([])
+        for lower, upper in product(lowers, uppers):
+            result.update(self._fill_range(lower, upper))
+        return result
+    
+    def fill_pattern_range(self, lower, upper):
+        lowers = self.match_pattern(lower)
+        uppers = self.match_pattern(upper)
+        return self.fill_set_range(lowers, uppers)
+        
+    def filter(self, codes):
+        return filter(self.check, codes)
+
+@memoize
+def create_parser(regex, pattern_matcher, range_filler, quote_pairs=[('\'','\''), ('"','"')], delimiters=[','], 
+                  require_quotes=False, require_delimiter=False):
+    code_pattern = Regex(regex)
+    code_pattern.setParseAction(lambda s, loc, toks: frozenset(pattern_matcher(toks[0])))
+    if require_quotes:
+        quoted_code_pattern = NoMatch()
+    else:
+        quoted_code_pattern = code_pattern
+    for opener, closer in quote_pairs:
+        quoted_code_pattern ^= Literal(opener).suppress() + code_pattern + Literal(closer).suppress()
+    
+    code_range = quoted_code_pattern + Literal('-').suppress() + quoted_code_pattern
+    code_range.setParseAction(lambda s, loc, toks: frozenset(range_filler(toks[0], toks[1])))
+    code_list_continuation = reduce(or_, map(Literal, delimiters)).suppress() + (code_range | quoted_code_pattern)
+    if not require_delimiter:
+        code_list_continuation |= White().suppress() + (code_range | quoted_code_pattern)
+    code_list = (code_range | quoted_code_pattern) + ZeroOrMore(code_list_continuation) + StringEnd()
+    return code_list
+
+all_quote_pairs = (('\'','\''), ('"','"'), ('‘','’'))
+class RegexVocabulary(Vocabulary):
+    def parse(self, expression,  quote_pairs=all_quote_pairs,
+              delimiters=(',',), require_quotes=False, require_delimiter=False):
+        parser = create_parser(self.regex, self.match_pattern, self.fill_set_range, quote_pairs=quote_pairs,
+                               delimiters=delimiters, require_quotes=require_quotes, require_delimiter=require_delimiter)
+        return set(chain(*parser.parseString(expression)))
     
 
+class LexiconVocabulary(Vocabulary):
+    def check(self, code):
+        return code in self.lexicon
+
+
+# class ParserMeta(type):
+#     def __init__(cls, name, bases, dct):
+#         super(ParserMeta, cls).__init__(name, bases, dct)
+#         
+# 
+# class ParserVocabulary(Vocabulary):
+#     def __init__(self, ):
+#     '''
+#     Subclasses should define 
+#     '''
+#     def parse(self, expression):
+#         self.parser.
+
     
+# 
+# class Vocabulary(object):
+#     @abstractmethod
+#     def parse(self, expression, delimiter=',;\s', range_delimiter='-'):
+#         delimiter_pattern = re.compile('[^%s]+' % delimiter)
+#         range_delimiter_pattern = re.compile(range_delimiter)
+#         
+#         # Remove any whitespace around range delimiters
+#         delimiter_range_delimiter_reduction_pattern = re.compile('\s*%s\s*' % range_delimiter)
+#         reduced_expression = delimiter_range_delimiter_reduction_pattern.sub(range_delimiter, expression)
+#         
+#         # Break into parts by delimiter
+#         parts = delimiter_pattern.findall(reduced_expression)
+#         
+#         # Now look for any ranges or wild cards
+#         codes = set()
+#         for raw_part in parts:
+#             # Remove quotes
+#             part = raw_part.replace('\'', '').replace('"', '').replace('’', '').replace('‘', '')
+#             
+#             # Handle ranges and patterns
+#             if range_delimiter_pattern.search(part):
+#                 raw_start, raw_end = re.split("'?\s*%s\s*'?" % range_delimiter, part)
+#                 starts = self.match_pattern(raw_start)
+#                 assert starts
+#                 ends = self.match_pattern(raw_end)
+#                 assert ends
+#                 for start in starts:
+#                     for end in ends:
+#                         codes.update(self.fill_range(start, end))
+#             else:
+#                 matches = self.match_pattern(part)
+#                 assert matches
+#                 codes.update(matches)
+#         return codes
+# #                 
+# #                 
+# #                 if '*' in start:
+# #                     starts = wild_card_fillers[(code_type, code_system)](start)
+# #                 else:
+# #                     starts = [start]
+# #                 if '*' in end:
+# #                     ends = wild_card_fillers[(code_type, code_system)](end)
+# #                 else:
+# #                     ends = [end]
+# #                 codes.extend(list(set(starts+ends+range_fillers[(code_type, code_system)](min(starts), max(ends)))))
+# #             elif '*' in part:
+# #                 codes.extend(wild_card_fillers[(code_type, code_system)](part))
+# #             else:
+# #                 codes.append(part)
+# #         return [code_processors[(code_type, code_system)](code) for code in codes]
+#     
+#     @abstractmethod
+#     def standardize(self, code):
+#         raise NotImplementedError
+#     
+#     @abstractmethod
+#     def _fill_range(self, lower, upper):
+#         raise NotImplementedError
+#     
+#     def fill_range(self, lower, upper):
+#         return [self.standardize(code) for code in self._fill_range(self.standardize(lower), self.standardize(upper))]
+#     
+#     @abstractmethod
+#     def _match_pattern(self, pattern):
+#         raise NotImplementedError
+#     
+#     def match_pattern(self, pattern):
+#         return [self.standardize(code) for code in self._match_pattern(self.standardize(pattern))]
+#     
+# 
+#     
--- clinvoc/ndc.py
+++ clinvoc/ndc.py
@@ -1,15 +1,96 @@
-from .base import Vocabulary
-from clinvoc.base import left_pad
+from .base import left_pad, RegexVocabulary, create_vocabulary_checker,\
+    LexiconVocabulary
+from operator import add
+from toolz.functoolz import compose
+from toolz.curried import partial
+from itertools import product
+import os
+from clinvoc.resources import resources
+import csv
 
-class NDC(Vocabulary):
-    @staticmethod
-    def _fill_range(start, end):
-        return [start, end]
+pattern = r'([\d\*]{1,5}-[\d\*]{1,4}-[\d\*]{1,2})|([\d\*]{1,11})'
+
+# def tup_to_code(tup):
+#     return '-'.join(tup)
+
+# def code_to_tup(code):
+#     if '-' in code:
+#         part1, part2, part3 = code.split('-')
+#         part1 = left_pad(part1, 5)
+#         part2 = left_pad(part2, 4)
+#         part3 = left_pad(part3, 2)
+#         return part1, part2, part3
+#     else:
+#         padded = left_pad(code, 11)
+#         
+#         has_letter = not code[-2].isdigit()
+#         return int(padded[:5], int(padded[5:9]), code[-2] if has_letter else '', int(padded[10:]) if has_letter else int(padded[9:])
+
+# def ints_to_int(ints):
+#     int1, int2, int3 = ints
+#     return (int1 * int(1e6)) + (int2 * int(1e2)) + int3
+
+# def int_to_code(integer):
+#     padded = left_pad(str(integer), 11)
+#     return '-'.join([padded[:5], padded[5:9], padded[9:]])
+
+def match_ndc_pattern(pattern):
+    return set(map(self.standardize, set(map(partial(reduce, add), product(*map(lambda x: [x] if x != '*' else list(map(str, range(10))), pattern))))))
+
+# def fill_ndc_range(lower, upper):
+#     lower_ = ints_to_int(code_to_ints(lower))
+#     upper_ = ints_to_int(code_to_ints(upper))
+#     return set(map(int_to_code, range(lower_, upper_+1)))
+
+# def standardize_ndc_code(code):
+#     return tup_to_code(code_to_tup(code))
+
+def code_to_tup(code):
+    if '-' in code:
+        part1, part2, part3 = code.split('-')
+        part1 = left_pad(part1, 5)
+        part2 = left_pad(part2, 4)
+        part3 = left_pad(part3, 2)
+        return part1 + part2 + part3
+    else:
+        raise ValueError
     
-    @staticmethod
-    def _match_pattern(pattern):
-        return [pattern]
+def tup_to_code(tup):
+    return ''.join(tup)
+
+with open(os.path.join(resources, 'ndctext', 'package.txt')) as infile:
+    reader = csv.reader(infile, delimiter='\t')
+    reader.next()
+    all_ndc_codes = []
+    for row in reader:
+        try:
+            all_ndc_codes.append(code_to_tup(row[2]))
+        except:
+            print row[2]
+            raise
+        
+all_ndc_codes.sort()
+all_ndc_codes = map(tup_to_code, all_ndc_codes)
     
-    @staticmethod
-    def standardize(code):
-        return left_pad(code, 11)
+class NDC(RegexVocabulary, LexiconVocabulary): # Diamond inheritance!
+    regex = r'([\d\*]{1,5}-[\d\*]{1,4}-[\d\*]{1,2})|([\d\*]{1,9}[a-zA-Z\d\*]{1,2})'
+    lexicon = set(all_ndc_codes)
+    def _match_pattern(self, pattern):
+        return set(map(self.standardize, 
+                       set(map(partial(reduce, add), product(*map(lambda x: [x] if x != '*' else list(map(str, range(10))), 
+                                                                  pattern))))))
+    
+    def standardize(self, code):
+        if '-' in code:
+            part1, part2, part3 = code.split('-')
+            part1 = left_pad(part1, 5)
+            part2 = left_pad(part2, 4)
+            part3 = left_pad(part3, 2)
+            return part1 + part2 + part3
+        else:
+            return left_pad(code, 11)
+    
+    def _fill_range(self, start, end):
+        raise NotImplementedError('NDC does not support range filling')
+    
+    
