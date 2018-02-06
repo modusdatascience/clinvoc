747d06c357ee4db3aa921d5067b3faf3b3bec963
Jason Rudy <jcrudy@gmail.com>
2018-02-06 12:13:10 -0800

    No frozendict

--- clinvoc/code_systems.py
+++ clinvoc/code_systems.py
@@ -1,4 +1,3 @@
-from frozendict import frozendict
 from clinvoc.icd10 import ICD10PCS, ICD10CM
 from clinvoc.icd9 import ICD9CM, ICD9PCS
 from clinvoc.hcpcs import HCPCS
@@ -9,15 +8,15 @@ from clinvoc.ubrev import UBREV
 def code_system_standardizers(icd10cm=dict(), icd10pcs=dict(), icd9cm=dict(),
                               icd9pcs=dict(), hcpcs=dict(), loinc=dict(),
                               ndc=dict(), ubrev=dict()):
-    result = frozendict(
-                        ICD10PCS = ICD10PCS(**icd10cm).standardize,
-                        ICD10CM = ICD10CM(**icd10pcs).standardize,
-                        ICD9CM = ICD9CM(**icd9cm).standardize,
-                        ICD9PCS = ICD9PCS(**icd9pcs).standardize,
-                        HCPCS = HCPCS(**hcpcs).standardize,
-                        LOINC = LOINC(**loinc).standardize,
-                        NDC = NDC(**ndc).standardize,
-                        UBREV = UBREV(**ubrev).standardize,
-                        )
+    result = dict(
+                  ICD10PCS = ICD10PCS(**icd10cm).standardize,
+                  ICD10CM = ICD10CM(**icd10pcs).standardize,
+                  ICD9CM = ICD9CM(**icd9cm).standardize,
+                  ICD9PCS = ICD9PCS(**icd9pcs).standardize,
+                  HCPCS = HCPCS(**hcpcs).standardize,
+                  LOINC = LOINC(**loinc).standardize,
+                  NDC = NDC(**ndc).standardize,
+                  UBREV = UBREV(**ubrev).standardize,
+                  )
     
     return result
